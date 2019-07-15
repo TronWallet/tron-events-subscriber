31030af19724afcbbc5539872e502ebaef39bb02
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2019-07-15 20:41:12 -0300

    adds topic to env var

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import * as ZeroMQ from 'zeromq';
 
 const {
-  TRON_EVENTS_URL
+  TRON_EVENTS_URL,
+  TRON_EVENTS_TOPIC
 } = process.env;
 
 const sock = ZeroMQ.socket('sub');
 
 sock.connect(TRON_EVENTS_URL);
-sock.subscribe('blockTrigger');
+sock.subscribe(TRON_EVENTS_TOPIC);
 
 sock.on('message', function(topic, message) {
   console.log(`received a message related to: ${topic} containing message: ${JSON.stringify(JSON.parse(message))}`);
